c54ad9ed686ce4edf907f97f8c6f56cda642056f
Sergey <sevalavin@edu.hse.ru>
2021-03-21 18:38:03 +0300

    Фикс отгрузки прямо из карточки Фикс исключения при отгрузке

---
+++ src/main/java/arsenal/metiz/AresenalMetiz/assets/SDeparture.java
@@ -0,0 +1,12 @@
+package arsenal.metiz.AresenalMetiz.assets;
+
+import arsenal.metiz.AresenalMetiz.models.SimpleDepartureObj;
+
+import java.util.List;
+
+public class SDeparture {
+
+    public SimpleDepartureObj data;
+    public String contrAgent;
+    public String account;
+}
--- src/main/java/arsenal/metiz/AresenalMetiz/service/departureservice/DepartureService.java
+++ src/main/java/arsenal/metiz/AresenalMetiz/service/departureservice/DepartureService.java
@@ -1,8 +1,10 @@
 package arsenal.metiz.AresenalMetiz.service.departureservice;
 
 import arsenal.metiz.AresenalMetiz.assets.IdToPrint;
+import arsenal.metiz.AresenalMetiz.assets.SDeparture;
 import arsenal.metiz.AresenalMetiz.models.MDeparture;
 import arsenal.metiz.AresenalMetiz.models.Position;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Set;
 
@@ -13,4 +15,6 @@ public interface DepartureService {
     Set<Position> doMultipleDeparture(String request, String except);
 
     IdToPrint confirmDeparture(MDeparture departure);
+
+    ResponseEntity<?> soloDeparture(SDeparture departure);
 }
